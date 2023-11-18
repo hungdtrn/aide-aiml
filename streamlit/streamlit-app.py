54ca2d6154209f310a446d98fa4b70d370dc03c2
randallholmes1999 <randallholmes@protonmail.com>
2023-11-18 14:55:14 +1100

    setting up chatbot functionality

--- streamlit/streamlit-app.py
+++ streamlit/streamlit-app.py
@@ -12,11 +12,26 @@ st.title("AIDe")
 # userId
 
 # First prompt
-prompt = st.text_input("Start journaling your thoughts")
+#prompt = st.text_input("Start journaling your thoughts")
+
 # Button
-if st.button('Generate response'):
-    response = "TEST STRING Sorry to hear that"
-    st.write(response)
+#if st.button('Generate response'):
+#    response = "TEST STRING Sorry to hear that"
+#    st.write(response)
+ 
+#Promt for Asking for a response 
+message = st.chat_message("assistant")
+message.write("Hi how may I help you")   
+    
+# Creating out a chat input
+prompt =  st.chat_input("Response.......")
+if prompt:
+    st.write(f"{prompt}")
+    
+#building out chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
 
 
 # response =
