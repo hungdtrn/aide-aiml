b715e202831ce7cd9528fc147898476780976bdd
Joseph Gulay <joegulay@gmail.com>
2023-11-18 14:02:07 +1100

    Create basic streamlit interface

--- streamlit/streamlit-app.py
+++ streamlit/streamlit-app.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.title("Hello World")
+# Page title
+st.title("AIDe")
+
+
+# First prompt
+prompt = st.text_input("Start journaling your thoughts")
+
+# Button
+if st.button('Generate response'):
+    #response code here
+    response = 'Sorry to hear that. Please tell me more'
+    st.write(response)
+
+# response =
+# Print response
