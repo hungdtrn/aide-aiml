8cd31ba7cc7fa725c2ab1ebb8ae654c600178294
Joseph Gulay <joegulay@gmail.com>
2023-11-18 14:21:23 +1100

    Update basic streamlit interface

--- streamlit/streamlit-app.py
+++ streamlit/streamlit-app.py
@@ -1,8 +1,15 @@
 import streamlit as st
-
+import requests
+import json
+from utils import createUser, chat, post
 # Page title
 st.title("AIDe")
 
+
+# Server URL
+
+# response
+# userId
 
 # First prompt
 prompt = st.text_input("Start journaling your thoughts")
@@ -10,8 +17,20 @@ prompt = st.text_input("Start journaling your thoughts")
 # Button
 if st.button('Generate response'):
     #response code here
-    response = 'Sorry to hear that. Please tell me more'
+    response = "TEST STRING Sorry to hear that"
     st.write(response)
 
 # response =
 # Print response
+
+
+
+
+
+
+
+
+
+    response = createUser("1", "shane", "01234567")
+    userId = response["userId"]
+    print(chat(userId, "good morning")['content'])
