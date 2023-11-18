888e3b883aad20167fdd4dbb32b6a45f5c45d010
Shane Antonio <shane.antonio@gmail.com>
2023-11-18 03:55:33 +0000

    Add test API

---
+++ server/server.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    return "Success"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug="True", port=8080)
