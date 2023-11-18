145899a9f0ef8bc9ae101e4b503795cd91c93c08
Hung Tran <hung.dtrn@gmail.com>
2023-11-18 15:21:23 +1100

    add createuser api

--- server/server.py
+++ server/server.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+import json
+from flask import Flask, jsonify, request, render_template
+
+import storage
 
 app = Flask(__name__)
 
 @app.route('/')
 def test():
     return "Success"
+
+@app.route('/createUser', methods=['POST'])
+def createUser():
+    storage.writeUser(request.json["userId"], request.json)
+    return json.dumps({"userId": request.json["userId"]})
 
 
 if __name__ == "__main__":
--- server/storage.py
+++ server/storage.py
@@ -1,8 +1,20 @@
 import json
 import os
 
+
+STORAGE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "./localStorage")
+
+def writeUser(userId, user):
+    upload(f"user/{userId}.json", json.dumps(user, indent=2))
+
 def upload(blob_name, content):
-    pass
+    """  Add ${content} to the file wih the path ${blob_name}
+    """
+    with open(os.path.join(STORAGE_PATH, blob_name), "w") as f:
+        json.dump(json.loads(content), f)
 
 def download(destination_blob_name):
-    pass
+    """ Load the content of a file with the path destination_blob_name
+    """
+    with open(os.path.join(STORAGE_PATH, destination_blob_name), "r") as f:
+        return json.load(f)
