acf887ea823bccb303e65f5267503a20beb6ba96
Shane Antonio <shaneantonio@gmail.com>
2023-11-19 10:55:06 +0000

    Add Dockerfile

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SERVER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-. $SCRIPT_DIR/../set_env.sh
+. $SERVER_DIR/../set_env.sh
 
+cp -r $SERVER_DIR/../ai $SERVER_DIR
 gcloud run deploy aide-server --project $PROJECT --region=$REGION --allow-unauthenticated \
     --update-secrets=OPENAI_API_KEY=OPENAI_API_KEY:latest \
     --set-env-vars "MODEL=gpt-3.5-turbo" \
     --set-env-vars "BUCKET_NAME=${BUCKET_NAME}" \
-    --source=$SCRIPT_DIR
+    --source=$SERVER_DIR
