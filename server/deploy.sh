8a100e8a0c7c9994c9feebe9ecc7e7ef653ca0c0
Shane Antonio <shaneantonio@gmail.com>
2023-11-19 11:49:19 +0000

    Add Docker build

--- server/deploy.sh
+++ server/deploy.sh
@@ -3,9 +3,14 @@ SERVER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 . $SERVER_DIR/../set_env.sh
 
-cp -r $SERVER_DIR/../ai $SERVER_DIR
+BUILD_DIR=$SERVER_DIR/../tmp_build
+rm -fr $BUILD_DIR
+mkdir $BUILD_DIR
+cp -r $SERVER_DIR/../ai $BUILD_DIR
+cp $SERVER_DIR/* $BUILD_DIR
 gcloud run deploy aide-server --project $PROJECT --region=$REGION --allow-unauthenticated \
     --update-secrets=OPENAI_API_KEY=OPENAI_API_KEY:latest \
     --set-env-vars "MODEL=gpt-3.5-turbo" \
     --set-env-vars "BUCKET_NAME=${BUCKET_NAME}" \
-    --source=$SERVER_DIR
+    --source=$BUILD_DIR
+rm -fr $BUILD_DIR
