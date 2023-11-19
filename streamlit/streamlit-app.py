107913722db84d0cd75b3bdf9b9eb0a08d9ad011
randallholmes1999 <randallholmes@protonmail.com>
2023-11-19 15:58:52 +1100

    moved some of the existing code into the utils app to clean existing code

--- streamlit/streamlit-app.py
+++ streamlit/streamlit-app.py
@@ -1,29 +1,13 @@
 import streamlit as st
 import requests
 import json
-# from utils import createUser, chat, post
+from utils import createUser, chat, post, stream
 # Page title
 st.title("AIDE")
 
 server_url = "http://127.0.0.1:8080"
-def post(path, obj):
-    headers = {'Content-type': 'application/json'}
-    response = requests.post(f"{server_url}/{path}", data=json.dumps(obj), headers=headers)
-    if not response.ok:
-        print(response.text)
-        raise Exception(f"Invalid response: {response.text}")
 
-    return json.loads(response.text)
 
-def stream(path, obj):
-    # CODE FOR STREAMING REQUESTS. Use when we want to stream token by token instead of showing the whole sentence.
-    headers = {'Content-type': 'application/json'}
-    response = requests.post(f"{server_url}/{path}", data=json.dumps(obj), headers=headers, stream=True)
-    if not response.ok:
-        print(response.text)
-        raise Exception(f"Invalid response: {response.text}")
-
-    return response.iter_content(None)
 # Server URL
 
 # response
@@ -44,7 +28,7 @@ message = st.chat_message("assistant")
 message.write(post("welcome", {"userId": 0})["msg"])
 
 # Streaming the chat
-prompt = st.chat_input("Response.......")
+prompt = st.chat_input("Type Message.......")
 if prompt:
     out_stream = stream("chat_stream", {'userId': 0, 'message': prompt})
     for line in out_stream:
--- streamlit/utils.py
+++ streamlit/utils.py
@@ -2,3 +2,21 @@ import requests
 import json
 
 # Write util files here such as createUser, chat, post and server url
+def post(path, obj):
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(f"{server_url}/{path}", data=json.dumps(obj), headers=headers)
+    if not response.ok:
+        print(response.text)
+        raise Exception(f"Invalid response: {response.text}")
+
+    return json.loads(response.text)
+
+def stream(path, obj):
+    # CODE FOR STREAMING REQUESTS. Use when we want to stream token by token instead of showing the whole sentence.
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(f"{server_url}/{path}", data=json.dumps(obj), headers=headers, stream=True)
+    if not response.ok:
+        print(response.text)
+        raise Exception(f"Invalid response: {response.text}")
+
+    return response.iter_content(None)
