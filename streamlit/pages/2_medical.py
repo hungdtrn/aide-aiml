a300c64c7ce3667cb64fc878342e1ee788883f9e
randallholmes1999 <randallholmes@protonmail.com>
2023-11-21 21:25:42 +1100

    implemented changes to incorporate server info from user and not dummy data

--- streamlit/pages/2_medical.py
+++ streamlit/pages/2_medical.py
@@ -37,50 +37,41 @@ userID = st.text_input('PatientID', None)
 
 if st.checkbox('View daily Summary'):
 
-    # response = post("retrievesummary", {"userId": userID})
-    response = get("dailySummary", {"userId": userID,
-                                    "n" : 5})
-    st.text(response)
+    response = get("dailySummary", {"userId": userID, "n" : 5})
+    df = pd.DataFrame.from_dict(response['response'])
+    
 
-    ##--Randall to do--##
-    # df = pd.DataFrame(response['history']) # Create a dataframe for the history
-    # df_summary= df[['date', 'developmentSummary']].tail(5).fillna("No information") # Take the last 5 entries, and fill NaN
-    # df_summary.loc[:, 'date'] = df_summary.loc[:, 'date'] = df_summary['date'].map(\
-    #                                 lambda x : str(pd.to_datetime(datetime.datetime.utcfromtimestamp(x)).date())\
+    df_summary= df[['date', 'summary']].tail(5).fillna("No information") # Take the last 5 entries, and fill NaN
+    df_summary = df_summary.replace('', 'No information')
+    # df_summary.loc['date'] = df_summary['date'].map(
+    #                                 lambda x : str(pd.to_datetime(datetime.datetime.utcfromtimestamp(x)).date())
     #                                     )# Convert to date time
-    ##------###
+    # Convert the 'date' column to datetime
+    df_summary['date'] = pd.to_datetime(df_summary['date'])
 
+    # Update the 'date' column with the date in string format
+    df_summary['date'] = df_summary['date'].dt.date.astype(str)
 
-    # tab1, tab2, tab3, tab4, tab5 = st.tabs(df_summary['date'].to_list()) # Pass in the datetimes as tab headers
+    
 
-    # tab_list = [tab1, tab2, tab3, tab4, tab5] # create a tab list
+    tab1, tab2 = st.tabs(df_summary['date'].to_list()) # Pass in the datetimes as tab headers
 
-    # # Populate the tabs
-    # for i in range(len(tab_list)):
-    #     with tab_list[i]:
-    #         st.header(df_summary['date'].iloc[i])
-    #         stx.scrollableTextbox(text = df_summary['developmentSummary'].iloc[i], border= True, key = i)
+    tab_list = [tab1, tab2] # create a tab list
 
+    # Populate the tabs
+    for i in range(len(tab_list)):
+        with tab_list[i]:
+            st.header(df_summary['date'].iloc[i])
+            stx.scrollableTextbox(text = df_summary['summary'].iloc[i], border= True, key = i)
+    
 
 if st.checkbox('View Indicator Trends'):
     #--- Indicator trends---#
     st.header("Indicator Trends")
 
-
-    # Create mock dates
-    dates = []
-    for i in range(5):
-        datetime_str = f'2{i}/11/23'
-        dates.append(datetime.datetime.strptime(datetime_str, '%d/%m/%y'))
-
-    # Create mock indicator data
-    df_indicators = pd.DataFrame()
-    df_indicators['date'] = dates
-    df_indicators['mentalHealth'] = [5,4,5,3,4]
-    df_indicators['socialHealth'] = [3, 3, 4, 5,5]
-    df_indicators['physicalHealth'] = [3,4,5,5,4]
-    date = df_indicators['date']
-    values = df_indicators[['mentalHealth','socialHealth','physicalHealth']]
+    indicator = get("indicator", {"userId": userID, "n" : 5})
+    df_indicators = pd.DataFrame.from_dict(indicator['response'])
+    df_indicators[['mental_health','physical_health','social_health']] = df_indicators['indicators'].apply(pd.Series)
 
     # Create radio buttons for graph
     indicator = st.radio(
@@ -89,7 +80,7 @@ if st.checkbox('View Indicator Trends'):
 
     if indicator == 'Mental health':
         fig, ax = plt.subplots(figsize=(8, 6))
-        ax.plot(df_indicators['date'], df_indicators['mentalHealth'], label = 'Mental Health', color = 'blue' )
+        ax.plot(df_indicators['date'], df_indicators['mental_health'], label = 'Mental Health', color = 'blue' )
         ax.set_ylim(0,5.5)
         ax.set_xlabel('Date')
         ax.legend()
@@ -98,7 +89,7 @@ if st.checkbox('View Indicator Trends'):
 
     elif indicator == 'Social health':
         fig, ax = plt.subplots(figsize=(8, 6))
-        ax.plot(df_indicators['date'], df_indicators['socialHealth'], label = 'Social Health', color = 'green')
+        ax.plot(df_indicators['date'], df_indicators['social_health'], label = 'Social Health', color = 'green')
         ax.set_ylim(0,5.5)
         ax.set_xlabel('Date')
         ax.legend()
@@ -108,7 +99,7 @@ if st.checkbox('View Indicator Trends'):
 
     elif indicator == 'Physical health':
         fig, ax = plt.subplots(figsize=(8, 6))
-        ax.plot(df_indicators['date'], df_indicators['physicalHealth'], label = 'Physical Health', color = 'orange')
+        ax.plot(df_indicators['date'], df_indicators['physical_health'], label = 'Physical Health', color = 'orange')
         ax.set_ylim(0,5.5)
         ax.set_xlabel('Date')
         ax.legend()
@@ -118,9 +109,9 @@ if st.checkbox('View Indicator Trends'):
 
     else:
         fig, ax = plt.subplots(figsize=(8, 6))
-        ax.plot(df_indicators['date'], df_indicators['mentalHealth'], label = 'Mental Health', color = 'blue' )
-        ax.plot(df_indicators['date'], df_indicators['socialHealth'], label = 'Social Health', color = 'green')
-        ax.plot(df_indicators['date'], df_indicators['physicalHealth'], label = 'Physical Health', color = 'orange')
+        ax.plot(df_indicators['date'], df_indicators['mental_health'], label = 'Mental Health', color = 'blue' )
+        ax.plot(df_indicators['date'], df_indicators['social_health'], label = 'Social Health', color = 'green')
+        ax.plot(df_indicators['date'], df_indicators['physical_health'], label = 'Physical Health', color = 'orange')
         ax.set_xlabel('Date')
         ax.set_ylim(0,5.5)
         ax.legend()
