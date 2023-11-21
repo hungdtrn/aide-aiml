9417a9a4cd573f1ad208c91a06a27d5734315ed8
Joe Gulay <joegulay@gmail.com>
2023-11-21 09:17:22 +0000

    Add set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,14 +1,14 @@
 #export OPENAI_API_KEY
 
-export PROJECT=
+export PROJECT=aide-aiml
 #PROJECT_ID=$(gcloud projects describe $PROJECT --format='value(projectNumber)')
 echo PROJECT_ID: $PROJECT_ID
-export PROJECT_ID=
+export PROJECT_ID=144023999182
 export REPO_HOME=~/git/aide-aiml
 
 #MODEL=gpt-4
 export MODEL=gpt-3.5-turbo
-export BUCKET_NAME=${PROJECT}-aide
+export BUCKET_NAME=joe-aide
 
 export REGION=australia-southeast2
 
