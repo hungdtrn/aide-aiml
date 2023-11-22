d6f85944a7a6b40695f1fd0f1804d8840221fd75
Joseph Gulay <joegulay@gmail.com>
2023-11-22 18:40:17 +1100

    update test_client.py

--- streamlit/test_client.py
+++ streamlit/test_client.py
@@ -6,12 +6,12 @@ from client import welcome, set_url
 
 def unit_tests():
     # Test local server
-    set_url("http://127.0.0.1:8080")
-    print(welcome( obj = {"userId": 0}))
-
-    # Test gcp server
-    # set_url("https://aide-server-ogdrzymura-km.a.run.app")
+    # set_url("http://127.0.0.1:8080")
     # print(welcome( obj = {"userId": 0}))
+
+    #Test gcp server
+    set_url("https://aide-server-ogdrzymura-km.a.run.app")
+    print(welcome( obj = {"userId": 0}))
 
     pass
 
