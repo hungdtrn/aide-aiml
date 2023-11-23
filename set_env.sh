1af0ab40fd68840579288153cda62895cf86cc75
Joseph Gulay <joegulay@gmail.com>
2023-11-23 21:54:17 +1100

    update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -5,7 +5,7 @@ export PROJECT=aide-aiml
 #PROJECT_ID=$(gcloud projects describe $PROJECT --format='value(projectNumber)')
 echo PROJECT_ID: $PROJECT_ID
 export PROJECT_ID=144023999182
-export REPO_HOME=/Users/joe/code/aide-aiml/
+export REPO_HOME=~/git/aide-aiml
 
 #MODEL=gpt-4
 export MODEL=gpt-3.5-turbo
